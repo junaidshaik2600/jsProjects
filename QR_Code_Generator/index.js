777e9d8bbf6761d74193e47281a3d6f46bf40031
junaidshaik2600 <junaidshaik2600@gmail.com>
2024-10-30 19:18:31 +0530

    Added logic & package.json

---
+++ QR_Code_Generator/index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      message: "Type in Your URL: ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    console.log("I am here" + url);
+    let qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("QRImage.png"));
+    let svg_string = qr.imageSync(url);
+    console.log(svg_string);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      console.log(error);
+      // Something else went wrong
+    }
+  });
